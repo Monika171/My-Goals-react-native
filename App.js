ec9e2edccb13931e67149a5187a39d78143e6d17
Monika171 <monikarabha171@gmail.com>
2022-11-24 14:28:51 +0900

    added image, fixed status bar

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import {useState} from 'react';
 import { StyleSheet, View, FlatList, Button } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 
@@ -12,8 +13,14 @@ export default function App() {
     setModalIsVisible(true)
   }
 
+  function endAddGoalHandler() {
+    setModalIsVisible(false);
+}
+
   function addGoalHandler(enteredGoalText){
     setCourseGoals(currentCourseGoals => [...courseGoals, {text: enteredGoalText, id: Math.random().toString()}]);
+    // setModalIsVisible(false);
+    endAddGoalHandler();
   }
 
   function deleteGoalHandler(id) {
@@ -23,9 +30,11 @@ export default function App() {
   }
 
   return (
+    <>
+    <StatusBar style='light'/>
     <View style={styles.appContainer}>
-      <Button title='Add New Goal' color="#5e0acc" onPress={startAddGoalHandler}/>
-      <GoalInput visible={modalIsVisible} onAddGoal={addGoalHandler}/>
+      <Button title='Add New Goal' color="#a065ec" onPress={startAddGoalHandler}/>
+      <GoalInput visible={modalIsVisible} onAddGoal={addGoalHandler} onCancel={endAddGoalHandler}/>
       <View style={styles.goalsContainer}>
       <FlatList data={courseGoals} renderItem={(itemData) => {
         return <GoalItem text={itemData.item.text} id={itemData.item.id} onDeleteItem={deleteGoalHandler}/>;
@@ -38,6 +47,7 @@ export default function App() {
       alwaysBounceVertical={false}/>
       </View>
     </View>
+    </>
   );
 }
 
@@ -45,7 +55,8 @@ const styles = StyleSheet.create({
   appContainer: {
     flex:1,
     paddingTop: 50,
-    paddingHorizontal: 16
+    paddingHorizontal: 16,
+    backgroundColor: '#1e085a'
   },
   goalsContainer: {
     flex: 5
